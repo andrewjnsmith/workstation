6d4663efbce3c8638a1a59699934b2e8f5dda241
Patrick Hindmarsh <patrick@hindmar.sh>
2013-01-29 14:21:54 +1300

    Added metadata and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## Pricemaker Workstation Default Setup
+
+To install these scripts you'll need the `soloist` gem, the [`pivotal_workstation` scripts](http://github.com/pivotal/pivotal_workstation) and the [`dmg` scripts](http://github.com/opscode-cookbooks/dmg)
---
+++ pricemaker/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Pricemaker"
+maintainer_email  "patrick@pricemaker.co.nz"
+license           "MIT"
+description       "Configure frequently-used tools for an Pricemaker OSX workstation"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+supports          "mac_os_x"
+depends           "pivotal_workstations"
